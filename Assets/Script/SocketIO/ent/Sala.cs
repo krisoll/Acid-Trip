640ac3278f4fdbdb4eae3b0628c121fd0608788f
sebmor93 <seb.mor.93@gmail.com>
2015-04-02 03:41:35 -0300

    Problema de casteo solucionado :D

---
+++ Assets/Script/SocketIO/ent/Sala.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Sala{
+	public string	nombre;
+	public string[] jugadores;
+	public string 	clave;
+	public string	distancia;
+	public int		estado;
+
+	public Sala(){}
+
+	public Sala(JSONObject jo){
+		if(jo.GetField ("nombre")!=null)
+			nombre 		= jo.GetField ("nombre").ToString ();
+		if(jo.GetField ("clave")!=null)
+			clave 		= jo.GetField ("clave").ToString ();
+		if (jo.GetField ("distancia") != null)
+			distancia = jo.GetField ("distancia").ToString ();
+		if(jo.GetField ("estado")!=null)
+			estado 		= int.Parse(jo.GetField ("estado").ToString ());
+		if (jo.GetField ("jugadores") != null) {
+			jo = jo.GetField ("jugadores");
+			List<JSONObject> lista = jo.list;
+			jugadores = new string[lista.Count];
+			for(int i=0;i<lista.Count;i++){
+				jugadores[i] = lista[i].ToString();
+			}
+		}
+	}
+
+}
